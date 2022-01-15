8d6756f2f6bf781a2e83234bdf5e5849ff2ee75b
joel-CM <cjoel1977@gmail.com>
2022-01-15 01:23:06 -0300

    add env_port

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(server.get("PORT"), () => {
+    console.log('%s listening at ' + server.get("PORT")); // eslint-disable-line no-console
   });
 });
